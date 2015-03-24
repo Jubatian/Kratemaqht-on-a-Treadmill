7851c1174b0ba868cebff1b4984e595fb60ca506
Sandor Zsuga <jubatian@jubatian.com>
2015-03-24 14:28:44 +0100

    Moved the scheme script into the crush script, so it is no longer necessary to add those to Gimp.

--- crush.sh
+++ crush.sh
@@ -5,7 +5,22 @@ if [ ! -d dragons ]; then
     exit 1
 fi
 echo "Note: the script will take a LOT of time for the entire dragon!"
-gimp -i -b '(batch-alpha-quantize "dragons/*.png" 64)' -b '(gimp-quit 0)'
+echo "(define pattern \"dragons/*.png\")"\
+     "(define colors 64)"\
+     "(let* ((filelist (cadr (file-glob pattern 1))))"\
+     "    (while (not (null? filelist))"\
+     "        (let* ((filename (car filelist))"\
+     "               (image (car (gimp-file-load RUN-NONINTERACTIVE filename filename)))"\
+     "               (drawable (car (gimp-image-get-active-layer image))))"\
+     "            (gimp-layer-add-mask drawable (car (gimp-layer-create-mask drawable ADD-ALPHA-TRANSFER-MASK)))"\
+     "            (gimp-image-convert-indexed image NO-DITHER MAKE-PALETTE colors FALSE FALSE \"\")"\
+     "            (gimp-image-convert-rgb image)"\
+     "            (gimp-layer-remove-mask drawable MASK-APPLY)"\
+     "            (gimp-file-save RUN-NONINTERACTIVE image drawable filename filename)"\
+     "            (gimp-image-delete image))"\
+     "        (set! filelist (cdr filelist))))"\
+     "(gimp-quit 0)"\
+     | gimp -i -b -
 mkdir dragons_crush
 pngcrush -d dragons_crush dragons/*.png
 rm -r dragons
